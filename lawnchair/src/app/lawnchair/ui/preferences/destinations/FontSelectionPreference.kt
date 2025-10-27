ae803c3bcdbf58b9ef309131d75f8cf2ab6a1833
tgex0 <160293965+tgex0@users.noreply.github.com>
2025-10-27 14:17:56 +0000

    Update FontSelectionPreference.kt

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
@@ -30,7 +30,7 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -330,10 +330,9 @@ private fun VariantDropdown(
         var showVariants by remember { mutableStateOf(false) }
 
         val context = LocalContext.current
-        DisposableEffect(family) {
+        LaunchedEffect(family) {
             val fontCache = FontCache.INSTANCE.get(context)
-            family.variants.forEach { fontCache.preloadFont(it.value) }
-            onDispose { }
+            family.sortedVariants.forEach { fontCache.preloadFont(it) }
         }
 
         TextButton(
