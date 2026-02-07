216222cbcfe6ba3fc2c48d4db92def39c21836bc
tgex0 <160293965+tgex0@users.noreply.github.com>
2026-02-07 22:55:59 +1300

    7 February 2026

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
@@ -78,14 +78,33 @@ fun FontSelection(
         list.add(FontCache.Family(FontCache.SystemFont("sans-serif")))
         list.add(FontCache.Family(FontCache.SystemFont("sans-serif-condensed")))
         list.add(FontCache.Family(FontCache.SystemFont("sans-serif-medium")))
-        GoogleFontsListing.INSTANCE.get(context).getFonts().mapTo(list) { font ->
+        val googleFamilies = GoogleFontsListing.INSTANCE.get(context).getFonts().map { font ->
             val variantsMap = HashMap<String, FontCache.Font>()
             val variants = font.variants.toTypedArray()
+
             font.variants.forEach { variant ->
-                variantsMap[variant] = FontCache.GoogleFont(context, font.family, variant, variants)
+                variantsMap[variant] =
+                    FontCache.GoogleFont(context, font.family, variant, variants)
             }
+
             FontCache.Family(font.family, variantsMap)
+        }.toMutableList()
+
+        val googleSansFlexIndex =
+            googleFamilies.indexOfFirst { it.family.equals("Google Sans Flex", true) }
+
+        val googleSansFlex =
+            if (googleSansFlexIndex != -1) googleFamilies.removeAt(googleSansFlexIndex)
+            else null
+
+        googleFamilies.sortBy { it.family.lowercase() }
+
+        googleSansFlex?.let {
+            googleFamilies.add(0, it)
         }
+
+        list.addAll(googleFamilies)
+
         value = list
     }
     val allItems by remember { derivedStateOf { items + customFonts } }
