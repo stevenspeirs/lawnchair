38e2774b1042d5aff9777106724562a71c061755
tgex0 <160293965+tgex0@users.noreply.github.com>
2026-02-07 21:39:18 +1300

    7 February 2026

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
@@ -79,19 +79,18 @@ fun FontSelection(
         list.add(FontCache.Family(FontCache.SystemFont("sans-serif-condensed")))
         list.add(FontCache.Family(FontCache.SystemFont("sans-serif-medium")))
         GoogleFontsListing.INSTANCE.get(context).getFonts()
-        
             .sortedWith(
                 compareByDescending<GoogleFont> { it.family == "Google Sans Flex" }
                     .thenBy { it.family },
-        )        
-        .mapTo(list) { font ->
-            val variantsMap = HashMap<String, FontCache.Font>()
-            val variants = font.variants.toTypedArray()
-            font.variants.forEach { variant ->
-                variantsMap[variant] = FontCache.GoogleFont(context, font.family, variant, variants)
+            )        
+            .mapTo(list) { font ->
+                val variantsMap = HashMap<String, FontCache.Font>()
+                val variants = font.variants.toTypedArray()
+                font.variants.forEach { variant ->
+                    variantsMap[variant] = FontCache.GoogleFont(context, font.family, variant, variants)
+                }
+                FontCache.Family(font.family, variantsMap)
             }
-            FontCache.Family(font.family, variantsMap)
-        }
         value = list
     }
     val allItems by remember { derivedStateOf { items + customFonts } }
