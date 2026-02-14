4ff0b8f87b1c6c8324ba6c449e3b7fd4cfcbcd6a
tgex0 <160293965+tgex0@users.noreply.github.com>
2026-02-14 14:44:19 +1300

    Update FontSelectionPreference.kt

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/FontSelectionPreference.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import app.lawnchair.font.FontAxes
