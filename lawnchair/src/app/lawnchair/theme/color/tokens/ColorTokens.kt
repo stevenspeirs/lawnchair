d0c45d5b2ff05dd2a2fbc5c3b4e58192588d340e
Pun Butrach <pun.butrach@gmail.com>
2026-03-03 22:44:33 +0700

    chore: Spotlessified
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/theme/color/tokens/ColorTokens.kt
+++ lawnchair/src/app/lawnchair/theme/color/tokens/ColorTokens.kt
@@ -98,9 +98,11 @@ object ColorTokens {
     @JvmField val AllAppsTabBackgroundSelected = DayNightColorToken(Accent1_600, Accent1_200)
 
     @JvmField val FocusHighlight = DayNightColorToken(Neutral1_0, Neutral1_700)
+
     @JvmField val FocusHighlightBlur = DayNightColorToken(Neutral1_0, Neutral1_700).setAlpha(.40f)
 
     @JvmField val GroupHighlight = Surface
+
     @JvmField val GroupHighlightBlur = Surface.setAlpha(.40f)
 
     @JvmField val OverviewScrimColor = DayNightColorToken(Neutral2_100.setLStar(87.0), Neutral1_800)
